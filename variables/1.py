6ec000c34adbddb01b830cb4703f27e4d377339a
prime-infinity <soe.a.prime@gmail.com>
2022-10-25 07:35:15 -0700

    variables

---
+++ variables/1.py
@@ -0,0 +1,14 @@
+# declare a variable and assign a value to it
+name = "osamede"
+
+# reassigning the variable to another value
+name = "prime"
+
+# weak typing means we can use completely different data type
+name = 4
+
+print(name)
+
+# bools
+is_god = True
+# remember python is case sensitive, so its "False", not "false" as in other langs
