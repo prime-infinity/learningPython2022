6dafbd640df1d17c9fdb41a9544f1b49bb205bd7
prime-infinity <soe.a.prime@gmail.com>
2022-10-26 12:42:42 -0700

    conditionals

---
+++ README.md
@@ -0,0 +1,1 @@
+Learning python as my fourth language after PHP, javascript and golang, in that order, needless to say, the codes in here may not be beginner friendly.But python is high level enough for most people to understand
---
+++ conditionals/1.py
@@ -0,0 +1,12 @@
+# if statements
+age = 35
+
+if age > 30:
+    # note and notice that python works with indentation, a poor design choice if you ask me
+    print("you are old")
+elif age < 30:
+    print("you are still young, congrats")
+else:
+    print("we dont know what you are")
+# end of if statement
+print("done")
