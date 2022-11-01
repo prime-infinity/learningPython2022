4485ce7fcf85dc28a98f1ea678217466dddb4a34
prime-infinity <soe.a.prime@gmail.com>
2022-11-01 15:08:25 -0700

    turples

---
+++ turples/1.py
@@ -0,0 +1,10 @@
+# right off the bat, turples are simply immutabel arrays/lists
+numbers = (1, 2, 3)
+print(numbers)
+# numbers[1] = 3 #note: this will not work as they cannot be mutated
+
+# turple methods
+numbers.count(1)  # return the number of occurence of an element
+
+numbers.index(2)
+# return the index position of the first occurence of an element
