4bf02c02b0ce0d29e748f61070a99d023761df25
prime-infinity <soe.a.prime@gmail.com>
2022-11-03 12:17:51 -0700

    formatted string

--- strings/2.py
+++ strings/2.py
@@ -3,4 +3,15 @@ message = ''' this is the message
 and it can be in multiple lines,
 this is because of the three quotes.'''
 
-print(message)
+# print(message)
+
+# strings are also treated like list/arrays
+course = "python course"
+print(course[8])  # o
+print(course[0:4])  # pyth
+
+# formatted string
+first = "john"
+last = "smith"
+msg = f'{first} {last} is a coder'
+print(msg)
