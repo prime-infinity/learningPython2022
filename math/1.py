c8a59b18398bf8bbfbabd37bf80c6d6c54b340b5
prime-infinity <soe.a.prime@gmail.com>
2022-11-05 12:08:09 -0700

    math module basics

---
+++ math/1.py
@@ -0,0 +1,10 @@
+# various math functions for dealing with numbers, some are in the math module
+
+import math
+
+x = 2.9
+print(round(x))
+print(abs(-29.3))
+
+print(math.ceil(x))
+print(math.floor(x))
