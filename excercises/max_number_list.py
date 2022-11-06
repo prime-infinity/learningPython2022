c7bcf46f7a12b026bd4f7b5c31d1c2ece5da18f0
prime-infinity <soe.a.prime@gmail.com>
2022-11-06 15:29:21 -0800

    excercise, max_number

---
+++ excercises/max_number_list.py
@@ -0,0 +1,11 @@
+# find the max number of a list
+numbers = [2, 5, 1, 7, 8, 9, 22, 63, 38, 84, 12, ]
+max_number = numbers[0]
+for n in numbers:
+    if n > max_number:
+        max_number = n
+
+print(max_number)
+
+# or just use the max() function
+print(max(numbers))
