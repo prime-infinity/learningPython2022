5a1ceaf151015c6fa78b00eb93377a5ebe42969c
prime-infinity <soe.a.prime@gmail.com>
2022-11-06 14:40:26 -0800

    nested loops

---
+++ excercises/total_number.py
@@ -0,0 +1,7 @@
+total = 0
+
+numbers = [10, 20, 30]
+for i in numbers:
+    total = total + i
+
+print(total)
---
+++ loops/3nested_loops.py
@@ -0,0 +1,8 @@
+# supposing we want to generate
+# (0,1),(0,2),(0,3),(1,0),(1,2),(1,3) ..
+
+for x in range(4):
+    #print("x coordinate set")
+    for y in range(4):
+        #print("y coordniate set")
+        print(f'({x},{y})')
