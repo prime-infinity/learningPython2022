7a2984f54fdfc08fa1b9875a7c2bcf2f71368d66
prime-infinity <soe.a.prime@gmail.com>
2022-11-06 15:39:33 -0800

    2d lists

---
+++ list/32D_lists.py
@@ -0,0 +1,15 @@
+# to define a matrix
+'''
+    [
+        [1,2,3],
+        [4,5,6],
+        [7,8,9]
+    ]
+'''
+
+martix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+print(martix[1][0])
+
+for row in martix:
+    for col in row:
+        print(col)
