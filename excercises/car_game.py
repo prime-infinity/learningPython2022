9a7543d00acd9ad724c4f88b8260e45ac2b395ef
prime-infinity <soe.a.prime@gmail.com>
2022-11-06 07:45:29 -0800

    car game extended, own imp

--- excercises/car_game.py
+++ excercises/car_game.py
@@ -1,16 +1,26 @@
-command = "s"
+command = ""
+started = ""
 
-while command != "quit":
+while command != "quit":  # the mans imple uses while True, to run until broken
     command = input("> ").lower()
     if command == "help":
-        print(''' start - start the car
-                  stop - stop the car
-                  quit - end the game
+        print(''' 
+start - start the car
+stop - stop the car
+quit - end the game
                   ''')
     elif command == "start":
-        print("car started")
+        if started:
+            print("car already started")
+        else:
+            started = True
+            print("car started")
     elif command == "stop":
-        print("car stopped")
+        if started == False:
+            print("car already stopped")
+        else:
+            print("car stopped")
+            started = False
     elif command == "quit":
         print("game ended")
         break
