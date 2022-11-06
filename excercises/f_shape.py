336d90a71b47cb8c029c02703610262bc2479497
prime-infinity <soe.a.prime@gmail.com>
2022-11-06 15:11:06 -0800

    print f with * challange,tuts imp

--- excercises/f_shape.py
+++ excercises/f_shape.py
@@ -2,5 +2,13 @@
 
 list = [5, 2, 5, 2, 2]
 
+# own imp
 for n in list:
     print(n*"*")
+
+# tuts imp
+for n in list:
+    output = ''
+    for i in range(n):
+        output += 'X'
+    print(output)
