4cad65a48698d3637b5d5080fec4d5539314b017
prime-infinity <soe.a.prime@gmail.com>
2022-11-11 09:08:02 -0800

    more on inheritance

--- class/inheritance/1.py
+++ class/inheritance/1.py
@@ -1,12 +1,15 @@
 # share or inherit similar behaviors
 
 class Mammal:
-    def walk(self):
-        print("is walking")
+    def walk(self, breed):
+        print(f'{breed} is walking')
 
 
 # will inherit all methods of mammal class,thus becoming its child
 class Dog(Mammal):
+    def __init__(self, breed):
+        self.breed = breed
+
     def bark(self):
         print("dog is barking")
 
@@ -15,6 +18,7 @@ class Cat(Mammal):
     pass  # tell the interpreter to fuck off
 
 
-german_shephard = Dog()
-german_shephard.walk()  # inherited method
-german_shephard.bark()
+german_shephard = Dog("german_sephard")
+german_shephard.walk(german_shephard.breed)  # inherited method
+# german_shephard.bark()
+# print()
