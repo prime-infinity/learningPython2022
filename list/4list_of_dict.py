e243dac1c8993a2dfdeaed4b17c23b3e78de61b4
prime-infinity <soe.a.prime@gmail.com>
2022-11-15 10:06:19 -0800

    list of dict

---
+++ list/4list_of_dict.py
@@ -0,0 +1,7 @@
+list_of_dict = [{"name": "prmie", "age": 12342, "role": "jedi"}, {"name": "darth", "age": "infinite",
+                                                                  "role": "cannot be discussed"}, {"name": "sidius", "role": "convincer", "age": "unknown"}]
+
+# print(list_of_dict)
+
+for li in list_of_dict:
+    print(li["name"])
