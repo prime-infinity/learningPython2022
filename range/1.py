419bf641317a9f2595ed789bc05e0e884b648a9f
prime-infinity <soe.a.prime@gmail.com>
2023-01-05 02:43:39 -0800

    minor changes inrangelist

--- range/1.py
+++ range/1.py
@@ -1,9 +1,14 @@
 numbers = range(10)  # create an array from 0 to 9
-
+list = [2, 2, 3, 4, 5, 6, 2]
 # we can then iterate over them
+'''
 for n in numbers:
     print(n)
+'''
 
-
+'''
 for i in range(5, 15, 5):
     print(i)
+'''
+for i in range(0, len(list)):
+    print(list[i])
